966e648a2b7316a6d7b99b041cc546680f9fc458
IB-art <65600199+IB-art@users.noreply.github.com>
2021-05-17 23:26:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  
+  $('.language__btn').on('click', function(){
+    $('.language__menu').slideToggle();
+    });
+
+    
+  $('.menu__flor').on('click', function(){
+    $('.flor-box').slideToggle();
+    });
+
+  $('.menu__regalo').on('click', function(){
+    $('.regalo-box').slideToggle();
+    });
+  
+
+});
